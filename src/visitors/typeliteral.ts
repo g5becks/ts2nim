a2cf01e1cdb362147b4d7256ef00555cdb516bf6
Gary Becks <techstar.dev@hotmail.com>
2020-11-02 12:31:09 -0500

    update methodSignatureVisitor to handle list of signatures

--- src/visitors/typeliteral.ts
+++ src/visitors/typeliteral.ts
@@ -1,6 +1,10 @@
 import { Node, TypeLiteralNode } from 'ts-morph'
+import { visit } from './visit'
 
 export const typeLiteralVisitor = (node: Node | Node[], parentName?: string): string => {
     const n = node as TypeLiteralNode
+    if (n.getMethods().length) {
+        const methods = visit(n.getMethods(), parentName)
+    }
     return n.getMethods()[0].getKindName()
 }
