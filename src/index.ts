ff68cfb7b2c516845efe8af3cceff9f3b928ab9d
Gary Becks <techstar.dev@hotmail.com>
2020-11-04 16:38:01 -0500

    delete visitors folder

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,11 @@
-import { Project } from 'ts-morph'
+import { Project, SourceFile } from 'ts-morph'
+import { visit } from './visitors'
 
 const proj = new Project({ tsConfigFilePath: './tsconfig.json' })
 
-const file = proj.getSourceFiles()[0]
+export const file = proj.getSourceFiles()[0]
+export const generate = (file: SourceFile): string =>
+    file
+        .forEachChildAsArray()
+        .map((child) => visit(child))
+        .join()
--- src/visitors/index.ts
+++
@@ -1,8 +0,0 @@
-import { SourceFile } from 'ts-morph'
-import { visit } from './visitors'
-
-export const generate = (file: SourceFile): string =>
-    file
-        .forEachChildAsArray()
-        .map((child) => visit(child))
-        .join()
