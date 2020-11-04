7e2261d5d17ed616d10636ea296bf8b223526bfc
Gary Becks <techstar.dev@hotmail.com>
2020-11-04 16:41:13 -0500

    move all utility functions into visitors.ts file

--- src/visitors.ts
+++ src/visitors.ts
@@ -21,15 +21,6 @@ import {
     VariableDeclarationKind,
 } from 'ts-morph'
 
-import {
-    buildFFiParams,
-    buildParams,
-    buildReturnType,
-    buildTypeName,
-    buildTypeParams,
-    buildVarName,
-    hasTypeParam,
-} from './utils'
 import events from 'events'
 
 const typesMap = new Map<string, string>([
@@ -592,3 +583,124 @@ const visitorMap = new Map<number, NodeVisitor>([
     [SyntaxKind.FirstJSDocTagNode, pass], // TODO create visitor
     [SyntaxKind.LastJSDocTagNode, pass], // TODO create visitor
 ])
+
+const nimReserved = [
+    'addr',
+    'and',
+    'as',
+    'asm',
+    'bind',
+    'block',
+    'break',
+    'case',
+    'cast',
+    'concept',
+    'const',
+    'continue',
+    'converter',
+    'defer',
+    'discard',
+    'distinct',
+    'div',
+    'do',
+    'elif',
+    'else',
+    'end',
+    'enum',
+    'except',
+    'export',
+    'finally',
+    'for',
+    'from',
+    'func',
+    'if',
+    'import',
+    'in',
+    'include',
+    'interface',
+    'is',
+    'isnot',
+    'iterator',
+    'let',
+    'macro',
+    'method',
+    'mixin',
+    'mod',
+    'nil',
+    'not',
+    'notin',
+    'object',
+    'of',
+    'or',
+    'out',
+    'proc',
+    'ptr',
+    'raise',
+    'ref',
+    'return',
+    'shl',
+    'shr',
+    'static',
+    'template',
+    'try',
+    'tuple',
+    'type',
+    'using',
+    'var',
+    'when',
+    'while',
+    'xor',
+    'yield',
+    'Object',
+]
+
+const capitalize = (text: string): string => text.replace(/^\w/, (c) => c.toUpperCase())
+/*
+const lowerCase = (text: string): string => text.replace(/^\w/, (c) => c.toLowerCase())
+*/
+const isReservedWord = (word: string): boolean => nimReserved.includes(word)
+
+const hasTypeParam = (node: Node): boolean => node.getChildrenOfKind(SyntaxKind.TypeParameter).length > 0
+
+const buildVarName = (v: string): string => {
+    const varName = v.trim()
+    return isReservedWord(varName) ? `js${capitalize(varName)}` : varName
+}
+
+const buildTypeName = (node: ClassDeclaration | TypeAliasDeclaration | string): string => {
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    if (typeof node === 'string') {
+        return isReservedWord(node) ? `Js${Node}` : node
+    }
+    const name = node.getNameNode()!.getText().trim()
+    return isReservedWord(name) ? `Js${capitalize(name)}` : capitalize(name)
+}
+
+// Builds type params for nodes that accept them.
+const buildTypeParams = (
+    node: FunctionDeclaration | FunctionTypeNode | MethodSignature | TypeAliasDeclaration,
+): string =>
+    node.getTypeParameters().length
+        ? `[${node
+              .getTypeParameters()
+              .map((p) => visit(p))
+              .join(', ')}]`
+        : ''
+
+// Builds function parameters for functionLike nodes
+const buildParams = (node: FunctionTypeNode | FunctionDeclaration | MethodSignature): string =>
+    node
+        .getParameters()
+        .map((param) => visit(param))
+        .join(', ')
+
+// Builds return type for functionLike nodes
+const buildReturnType = (node: FunctionTypeNode | FunctionDeclaration | MethodSignature): string =>
+    node.getReturnTypeNode() ? visit(node.getReturnTypeNodeOrThrow()) : 'any'
+
+// Builds FFI params for functionLike nodes
+const buildFFiParams = (node: FunctionDeclaration | FunctionTypeNode | MethodSignature): string =>
+    node
+        .getParameters()
+        .map((p) => (p.isRestParameter() ? '...#' : '#'))
+        .join(', ')
