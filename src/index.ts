8ae4acec49d78f2dce37323a11df800537ee894c
g5becks <techstar.dev@hotmail.com>
2020-11-07 14:44:34 -0500

    update generate function

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,16 @@
-import { Project, SourceFile } from 'ts-morph'
-import { LiteralToBuild, visit } from './visitors'
+import { Project } from 'ts-morph'
+import { initLiteralsMap, visit } from './visitors'
 
-const proj = new Project({ tsConfigFilePath: './tsconfig.json' })
-
-export const file = proj.getSourceFiles()[0]
-export const generate = (file: SourceFile): string => {
-    const literalSet = new Set<LiteralToBuild>()
-    const data = file
-        .forEachChildAsArray()
-        .map((child) => visit(child, '', literalSet))
-        .join('\n')
-
-    let typesToBuild = ''
-    for (const t of literalSet.values()) {
-        typesToBuild = typesToBuild.concat(`type ${t.name} = ${t.type}
-        `)
+export const generate = (proj: Project): { out: string; source: string }[] => {
+    initLiteralsMap(proj.getSourceFiles())
+    let convertedMap: { out: string; source: string }[] = []
+    for (const file of proj.getSourceFiles()) {
+        const data = file
+            .forEachChildAsArray()
+            .map((child) => visit(child))
+            .join('\n')
+        convertedMap = [...convertedMap, { out: file.getBaseNameWithoutExtension() + '.nim', source: data }]
     }
-    console.log(typesToBuild)
-    return typesToBuild.concat(data)
+
+    return convertedMap
 }
