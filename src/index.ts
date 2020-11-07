3528c80290188b893c834e42a709d322c3e0be65
g5becks <techstar.dev@hotmail.com>
2020-11-07 14:49:30 -0500

    change return type of generate function

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,24 @@
 import { Project } from 'ts-morph'
-import { initLiteralsMap, visit } from './visitors'
+import { getTypesToBuild, initLiteralsMap, visit } from './visitors'
 
 export const generate = (proj: Project): { out: string; source: string }[] => {
     initLiteralsMap(proj.getSourceFiles())
     let convertedMap: { out: string; source: string }[] = []
     for (const file of proj.getSourceFiles()) {
-        const data = file
-            .forEachChildAsArray()
-            .map((child) => visit(child))
-            .join('\n')
-        convertedMap = [...convertedMap, { out: file.getBaseNameWithoutExtension() + '.nim', source: data }]
+        const out = file.getBaseNameWithoutExtension() + '.nim'
+        let source = ''
+        for (const type of getTypesToBuild(file).values()) {
+            source = source.concat(`type ${type.name} = ${type.type}
+            `)
+        }
+        source = source.concat(
+            file
+                .forEachChildAsArray()
+                .map((child) => visit(child))
+                .join('\n'),
+        )
+
+        convertedMap = [...convertedMap, { out, source }]
     }
 
     return convertedMap
