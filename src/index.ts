38f1b205585c8498f1e1fb0f897670b421edc7ee
g5becks <techstar.dev@hotmail.com>
2020-11-07 14:54:23 -0500

    update generate function params

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
-import { Project } from 'ts-morph'
+import { Project, SourceFile } from 'ts-morph'
 import { getTypesToBuild, initLiteralsMap, visit } from './visitors'
 
-export const generate = (proj: Project): { out: string; source: string }[] => {
-    initLiteralsMap(proj.getSourceFiles())
+export const generate = (files: SourceFile[]): { out: string; source: string }[] => {
+    initLiteralsMap(files)
     let convertedMap: { out: string; source: string }[] = []
-    for (const file of proj.getSourceFiles()) {
+    for (const file of files) {
         const out = file.getBaseNameWithoutExtension() + '.nim'
         let source = ''
         for (const type of getTypesToBuild(file).values()) {
